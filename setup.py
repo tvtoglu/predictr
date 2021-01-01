38eeeaf96e0830a07a1368f9b7583e3346701d9a
tmroo <tamer.tevetoglu@outlook.com>
2021-01-01 14:17:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,14 +10,13 @@ setup(
       author='Tamer Tevetoglu',
       author_email="tamer.tevetoglu@ima.uni-stuttgart.de",
       url="https://tvtoglu.github.io/predictr/",
-      py_modules=["predictr", "plot_all"],
+      py_modules=["predictr"],
       package_dir={'': 'src'},
       long_description=long_description,
       long_description_content_type='text/markdown',
-      keywords = 'reliability, weibull, bias'
+      keywords = 'reliability, weibull, bias',
       classifiers=[
           "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7",
           "Programming Language :: Python :: 3.8",
           "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
