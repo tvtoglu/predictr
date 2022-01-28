a01754923b90ba589209d1b1c11bd8d5f14e5255
tvtoglu <45008505+tvtoglu@users.noreply.github.com>
2022-01-28 22:09:23 +0100

    change license to MIT

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
           "Programming Language :: Python :: 3.8",
           "Programming Language :: Python :: 3.9",
           "Programming Language :: Python :: 3.10",
-          "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+          "License :: OSI Approved :: MIT License",
           "Operating System :: OS Independent",],
       install_requires= [
           "numpy >= 1.16.0",
