41e22b3681b4843cb53ec6ad46811c60ec1bea20
tvtoglu <45008505+tvtoglu@users.noreply.github.com>
2023-03-01 22:00:48 +0100

    minor updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='predictr',
-      version='0.1.26',
+      version='0.1.27',
       description='Life Data Analysis for Reliability Engineers - Weibull Analysis, Detailed Plots, Compute Statistics',
       author='Tamer Tevetoglu',
       author_email="predictr@outlook.com",
